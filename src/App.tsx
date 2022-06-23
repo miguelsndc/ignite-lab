fc7bad9661d948584a514db8090f7b54e2754ed0
miguelsndc <miguelsndc>
2022-06-23 15:33:13 -0300

    feat: components; event page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { gql, useQuery } from '@apollo/client'
+import { Event } from './pages/Event'
 import './styles/global.css'
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      teacher {
-        id
-        name
-        bio
-      }
-    }
-  }
-`
-
 export function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY)
-
-  console.log(data)
-
   return (
     <div>
-      <h1>Hello World</h1>
+      <Event />
     </div>
   )
 }
